8f688d33e2dae3a0e00a28c1e82e7543310e8892
kzhitter <133650064+kzhitter@users.noreply.github.com>
2026-09-08 23:38:22 +0800

    changed correct code for navigator.py

--- develop/toml_to_navigator.py
+++ develop/toml_to_navigator.py
@@ -1,52 +1,122 @@
-import requests
-import os
 import tomllib
+import os
 
-url = "https://detectionengineering101.kb.us-central1.gcp.cloud.es.io:9243/api/detection_engine/rules"
-api_key = os.environ['ELASTIC_API_KEY']
-headers = {
-    'Content-Type': 'application/json;charset=UTF-8',
-    'kbn-xsrf': 'true',
-    'Authorization': 'ApiKey ' + api_key
-}
-
-data = ""
+techniques = {}
 
 for root, dirs, files in os.walk("detections/"):
     for file in files:
-        data = "{\n"
         if file.endswith(".toml"):
             full_path = os.path.join(root, file)
             with open(full_path,"rb") as toml:
                 alert = tomllib.load(toml)
 
-            if alert['rule']['type'] == "query": # query based alert
-                required_fields = ['author','description', 'name','rule_id','risk_score','severity','type','query','threat']
-            elif alert['rule']['type'] == "eql": # event correlation alert
-                required_fields = ['author','description', 'name','rule_id','risk_score','severity','type','query','language','threat']
-            elif alert['rule']['type'] == "threshold": # threshold based alert
-                required_fields = ['author','description', 'name','rule_id','risk_score','severity','type','query','threshold','threat']
-            else:
-                print("Unsupported rule type found in: " + full_path)
-                break
-            
-            for field in alert['rule']:
-                if field in required_fields:
-                    if type(alert['rule'][field]) == list:
-                        data += "  " + "\"" + field + "\": " + str(alert['rule'][field]).replace("'","\"") + "," + "\n"
-                    elif type(alert['rule'][field]) == str:
-                        if field == 'description':
-                            data += "  " + "\"" + field + "\": \"" + str(alert['rule'][field]).replace("\n"," ").replace("\"","\\\"").replace("\\","\\\\") + "\"," + "\n"
-                        elif field == 'query':
-                            data += "  " + "\"" + field + "\": \"" + str(alert['rule'][field]).replace("\\","\\\\").replace("\"","\\\"").replace("\n"," ") + "\"," + "\n"
-                        else:
-                            data += "  " + "\"" + field + "\": \"" + str(alert['rule'][field]).replace("\n"," ").replace("\"","\\\"") + "\"," + "\n"
-                    elif type(alert['rule'][field]) == int:
-                        data += "  " + "\"" + field + "\": " + str(alert['rule'][field]) + "," + "\n"
-                    elif type(alert['rule'][field]) == dict:
-                        data += "  " + "\"" + field + "\": " + str(alert['rule'][field]).replace("'","\"") + "," + "\n"
-                    
-            data += "  \"enabled\": true\n}"
+                for threat in alert['rule']['threat']:
+                    technique_id = str(threat['technique'][0]['id'])
+                    tactic = str(threat['tactic']['name'])
 
-        elastic_data = requests.post(url, headers=headers, data=data).json()
-        print(elastic_data)
+                    if technique_id not in techniques:
+                        obj = {'technique_id': technique_id, 'tactic': tactic, 'count': 1}
+                        techniques[technique_id] = obj
+                    else:
+                        techniques[technique_id]['count'] += 1
+                    
+                    if 'subtechnique' in threat['technique'][0]:
+                        subtechnique = threat['technique'][0]['subtechnique'][0]['id']
+                        if subtechnique not in techniques:
+                            obj = {'technique_id': subtechnique, 'tactic': tactic, 'count': 1}
+                            techniques[subtechnique] = obj
+                        else:
+                            techniques[subtechnique]['count'] += 1
+
+
+beginning = """{
+	"name": "Custom Detections",
+	"versions": {
+		"attack": "13",
+		"navigator": "4.8.2",
+		"layer": "4.4"
+	},
+	"domain": "enterprise-attack",
+	"description": "",
+	"filters": {
+		"platforms": [
+			"Linux",
+			"macOS",
+			"Windows",
+			"Network",
+			"PRE",
+			"Containers",
+			"Office 365",
+			"SaaS",
+			"Google Workspace",
+			"IaaS",
+			"Azure AD"
+		]
+	},
+	"sorting": 0,
+	"layout": {
+		"layout": "side",
+		"aggregateFunction": "average",
+		"showID": false,
+		"showName": true,
+		"showAggregateScores": false,
+		"countUnscored": false
+	},
+	"hideDisabled": false,
+    """
+
+end = """
+    ],
+	"gradient": {
+		"colors": [
+			"#ff6666ff",
+			"#ffe766ff",
+			"#8ec843ff"
+		],
+		"minValue": 0,
+		"maxValue": 3
+	},
+	"legendItems": [],
+	"metadata": [],
+	"links": [],
+	"showTacticRowBackground": false,
+	"tacticRowBackground": "#dddddd",
+	"selectTechniquesAcrossTactics": true,
+	"selectSubtechniquesWithParent": false
+}"""
+
+counter = 0
+total_techniques = 0
+
+for technique in techniques:
+    total_techniques += 1
+
+output_path = "metrics/navigator.json"
+
+outF = open(output_path, "w")
+outF.write(beginning)
+outF.write("\"techniques\": [")
+
+for key in techniques:
+    counter += 1
+    technique_id = techniques[key]['technique_id']
+    count = str(techniques[key]['count'])
+    tactic = techniques[key]['tactic'].lower()
+    outF.write("\n\t\t{")
+    outF.write("\n\t\t\t\"techniqueID\": \"" + technique_id + "\",")
+    outF.write("\n\t\t\t\"tactic\": \"" + tactic + "\",")
+    outF.write("\n\t\t\t\"score\": " + count + ",")
+    outF.write("\n\t\t\t\"color\": \"\""",")
+    outF.write("\n\t\t\t\"comment\": \"\""",")
+    outF.write("\n\t\t\t\"enabled\": true,")
+    outF.write("\n\t\t\t\"metadata\": [],")
+    outF.write("\n\t\t\t\"links\": [],")
+    outF.write("\n\t\t\t\"showSubtechniques\": false")
+
+    if counter != total_techniques:
+        outF.write("\n\t\t},")
+    else:
+        outF.write("\n\t\t}")
+
+outF.write(end)
+outF.close()
