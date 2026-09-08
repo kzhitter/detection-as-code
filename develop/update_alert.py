ffc283719cee4fcc02377e004b04e5acc00607ea
kzhitter <133650064+kzhitter@users.noreply.github.com>
2026-09-08 16:57:14 +0800

    Update update_alert.py

--- develop/update_alert.py
+++ develop/update_alert.py
@@ -59,4 +59,11 @@ for root,dirs,files in os.walk("detections/"):
                     url= url + "?rule_id=" + rule_id
                     
                     elastic_data= requests.put(url, headers=headers, data=data).json()
-                    print(elastic_data)
+                    
+                    for key in elastic_data:
+                        if key == "status_code":
+                            if 404 == elastic_data["status_code"]:
+                                elastic_data= requests.post(url, headers=headers, data=data).json()
+                                print(elastic_data)
+                    
+                    
